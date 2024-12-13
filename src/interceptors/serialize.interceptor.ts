49fb27f304764e9c14238147c0abbe75060523d3
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-13 21:14:40 +0100

    Use serialize interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,13 +1,15 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    console.log('Interceptor', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        console.log('Next handler', data);
+        return plainToInstance(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
