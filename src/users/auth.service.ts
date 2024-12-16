ad558639b09c9974ec519bb278feabf691437508
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-16 22:25:05 +0100

    Generate hashed password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { promisify } from 'util';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -11,5 +15,9 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('Email in use.');
     }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
   }
 }
