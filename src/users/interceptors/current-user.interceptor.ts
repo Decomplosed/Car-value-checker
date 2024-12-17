d574882c07c5257430c30d88a10e70d856fd95c9
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-17 16:55:03 +0100

    Assign currentUser to request

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -10,5 +10,15 @@ import { UsersService } from '../users.service';
 export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
 
-  async intercept(context: ExecutionContext, handler: CallHandler) {}
+  async intercept(context: ExecutionContext, handler: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session;
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+
+    return handler.handle();
+  }
 }
