51506d259ae12eac9f5b502e346410e81351baea
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-17 17:17:50 +0100

    Mock UsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,10 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
+    const usersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
     const module = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: usersService },
+      ],
     }).compile();
 
     const service = module.get(AuthService);
