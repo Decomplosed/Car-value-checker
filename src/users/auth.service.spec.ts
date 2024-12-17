6bc4d88d96cdc43f5066b43807ae5a15971074da
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-17 17:23:20 +0100

    Improve TS around mocked user service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
-    const usersService = {
-      find: () => Promise.resolve([]),
+    const mockedUsersService: Partial<UsersService> = {
+      findByEmail: () => Promise.resolve([]),
       create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password }),
+        Promise.resolve({ id: 1, email, password } as User),
     };
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
-        { provide: UsersService, useValue: usersService },
+        { provide: UsersService, useValue: mockedUsersService },
       ],
     }).compile();
 
