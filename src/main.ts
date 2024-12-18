54b426dbdca4649996eb1d674258447a1b613d09
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-18 21:22:28 +0100

    Apply useCookie inside app module

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['test123'],
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
