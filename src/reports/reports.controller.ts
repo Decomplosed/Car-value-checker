2d0c95c79a23efddab1a2655a3e804e3c3b897b2
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-19 22:35:55 +0100

    Use reportsService in controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
   @Post()
-  createReport(@Body body: CreateReportDto) {}
+  createReport(@Body() body: CreateReportDto) {}
 }
