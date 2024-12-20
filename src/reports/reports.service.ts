2b9f31ac1d367c4b90f552e2e525a98c8c5aa1b1
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-20 22:05:05 +0100

    Search report and throw error if not existing in db

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
@@ -14,5 +14,12 @@ export class ReportsService {
     report.user = user;
 
     return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id } });
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
   }
 }
